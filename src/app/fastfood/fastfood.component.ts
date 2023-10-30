afe6bb15337a451dd6ca8af3f0feb404f165cae9
sinasalman <sina.salman381@gmail.com>
2023-10-30 18:05:50 +0330

    add animation and category on fastfood page

--- src/app/fastfood/fastfood.component.ts
+++ src/app/fastfood/fastfood.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger, state, style, animate, transition, query, group
+} from '@angular/animations';
 @Component({
   selector: 'app-fastfood',
   templateUrl: './fastfood.component.html',
-  styleUrls: ['./fastfood.component.scss']
+  styleUrls: ['./fastfood.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({opacity :1}),
+        animate(1000, style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class FastfoodComponent {
 
